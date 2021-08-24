3e3f4695465e6e028c79dd0a2b34cd1d0c117b45
NathanYee412 <nyee@csu.fullerton.edu>
2021-08-23 23:20:32 -0700

    added background img to buttons

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-window.alert("Hello");
+let numOfDrumButtons = document.querySelectorAll(".drum").length;
+
+
+for(let i = 0; i < numOfDrumButtons; i++) {
+    document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+        this.style.color = 'white';
+    });
+}
+
+
+
+// Notes 
+// i < document.querySelectorAll(".drum").length
+//        - number of drum class buttons 
+
+// 
+// let sound = new Audio('/sounds/tom-1.mp3');
+// sound.play();
+//        - how to play a sound on html
