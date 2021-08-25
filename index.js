e9139b1264989800aa65ad29f211fa56f2298993
NathanYee412 <nyee@csu.fullerton.edu>
2021-08-24 20:36:22 -0700

    keydown and click triggers sounds to play

--- index.js
+++ index.js
@@ -3,9 +3,85 @@ let numOfDrumButtons = document.querySelectorAll(".drum").length;
 
 for(let i = 0; i < numOfDrumButtons; i++) {
     document.querySelectorAll(".drum")[i].addEventListener("click", function () {
-        this.style.color = 'white';
+        
+        var buttonInnerHtml = this.innerHTML;
+
+        switch (buttonInnerHtml) {
+            case "w":
+                let w = new Audio('/sounds/tom-1.mp3');
+                w.play();
+                break;
+            case "a":
+                let a = new Audio('/sounds/tom-2.mp3');
+                a.play();
+                break;
+            case "s":
+                let s = new Audio('/sounds/tom-3.mp3');
+                s.play();
+                break;
+            case "d":
+                let d = new Audio('/sounds/tom-4.mp3');
+                d.play();
+                break;
+            case "j":
+                let j = new Audio('/sounds/snare.mp3');
+                j.play();
+                break;
+            case "k":
+                let k = new Audio('/sounds/crash.mp3');
+                k.play();
+                break;
+            case "l":
+                let l = new Audio('/sounds/kick-bass.mp3');
+                l.play();
+                break;
+
+            default:
+                break;
+        }
+        
+
     });
 }
+
+
+// entire document is listening for keyboard strokes 
+document.addEventListener("keydown", (event) => {
+    const keyname = event.key;
+    switch (keyname) {
+        case "w":
+            let w = new Audio('/sounds/tom-1.mp3');
+            w.play();
+            break;
+        case "a":
+            let a = new Audio('/sounds/tom-2.mp3');
+            a.play();
+            break;
+        case "s":
+            let s = new Audio('/sounds/tom-3.mp3');
+            s.play();
+            break;
+        case "d":
+            let d = new Audio('/sounds/tom-4.mp3');
+            d.play();
+            break;
+        case "j":
+            let j = new Audio('/sounds/snare.mp3');
+            j.play();
+            break;
+        case "k":
+            let k = new Audio('/sounds/crash.mp3');
+            k.play();
+            break;
+        case "l":
+            let l = new Audio('/sounds/kick-bass.mp3');
+            l.play();
+            break;
+
+        default:
+            break;
+    }
+});
 
 
 
